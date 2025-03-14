613e6a6e43bdbdb47f90fa8d2432ce2a1491d65c
Deval <deval@arc10.io>
2025-03-14 13:03:40 -0400

    fix: upgrade node should take helm chart version into account too

--- application/service/version_service.py
+++ application/service/version_service.py
@@ -6,16 +6,15 @@ from fastapi import status
 
 from application.config.config import Config
 from application.exception.application_error import ApplicationError
-from application.utils.ssh_utils import get_ssh_client, run_ssh_command
+from application.utils.ssh_utils import run_ssh_command
 from application.utils.logger import log
-from application.model.machine_input import ControlMachineInput
 
 
 class VersionService:
     def __init__(self):
         self.HELM_CHECK_CMD = "helm list -n akash-services -o json | jq '.[] | select(.name == \"akash-node\")'"
 
-    def _get_system_version(self, ssh_client) -> str:
+    def _get_akash_node_helm_release_versions(self, ssh_client) -> Tuple[str, str]:
         """Get system version from helm release"""
         stdout, _ = run_ssh_command(ssh_client, self.HELM_CHECK_CMD, True)
         helm_data = json.loads(stdout)
@@ -30,41 +29,49 @@ class VersionService:
                 },
             )
 
-        return helm_data.get("app_version")
+        chart_version = helm_data.get("chart", "").replace("akash-node-", "")
+        app_version = helm_data.get("app_version")
+        return app_version, chart_version
 
     def _compare_versions(
-        self, system_version: str, current_version: str
+        self, current_version: str, desired_version: str
     ) -> Tuple[bool, str, str]:
         """Compare system and current versions"""
-        system_v = system_version.lstrip("v")
         current_v = current_version.lstrip("v")
+        desired_v = desired_version.lstrip("v")
 
-        needs_upgrade = version.parse(system_v) < version.parse(current_v)
+        needs_upgrade = version.parse(current_v) < version.parse(desired_v)
 
-        return needs_upgrade, current_v, system_v
+        return needs_upgrade, current_v, desired_v
 
     async def check_upgrade_status(self, ssh_client) -> Dict:
         """Check if network upgrade is needed"""
 
         def get_version():
             try:
-                return self._get_system_version(ssh_client)
+                app_version, chart_version = self._get_akash_node_helm_release_versions(ssh_client)
+                return app_version, chart_version
             finally:
                 ssh_client.close()
 
         try:
             # Get the system version in a separate thread
-            system_version = await asyncio.to_thread(get_version)
+            app_version, chart_version = await asyncio.to_thread(get_version)
 
-            # Compare versions
-            needs_upgrade, current_version, system_version = self._compare_versions(
-                system_version, Config.AKASH_VERSION
+            # Compare app and chart versions
+            app_needs_upgrade, app_current_version, app_desired_version = self._compare_versions(
+                app_version, Config.AKASH_VERSION
             )
+            chart_needs_upgrade, chart_current_version, chart_desired_version = self._compare_versions(
+                chart_version, Config.AKASH_NODE_HELM_CHART_VERSION
+            )
+            
+            needs_upgrade = app_needs_upgrade or chart_needs_upgrade
 
             return {
-                "needs_upgrade": needs_upgrade,
-                "current_network_version": current_version,
-                "system_version": system_version,
+                "needs_upgrade": True,
+                "app_version": {"current": app_current_version, "desired": app_desired_version},
+                "chart_version": {"current": chart_current_version, "desired": chart_desired_version},
             }
 
         except Exception as e:
@@ -82,7 +89,8 @@ class VersionService:
             check_upgrade_status = await self.check_upgrade_status(ssh_client)
             
             needs_upgrade = check_upgrade_status["needs_upgrade"]
-            current_version = check_upgrade_status["current_network_version"]
+            app_version = check_upgrade_status["app_version"]["desired"]
+            chart_version = check_upgrade_status["chart_version"]["desired"]
             if not needs_upgrade:
                 return {"status": "success", "message": "Network is up to date"}
             else:
@@ -92,7 +100,7 @@ class VersionService:
                 log.info("Updating Helm repositories...")
                 stdout, stderr = run_ssh_command(
                     ssh_client,
-                    "helm repo update",
+                    "helm repo update akash",
                     True,
                     task_id=task_id,
                 )
@@ -117,10 +125,10 @@ class VersionService:
                     )
 
                 # Upgrade akash-node deployment
-                log.info(f"Upgrading akash-node to version {current_version}...")
+                log.info(f"Upgrading akash-node to version {app_version}...")
                 stdout, stderr = run_ssh_command(
                     ssh_client,
-                    f"helm upgrade --install akash-node akash/akash-node -n akash-services --set image.tag={current_version}",
+                    f"kubectl delete pod -n akash-services -l app=akash-node",
                     True,
                     task_id=task_id,
                 )
