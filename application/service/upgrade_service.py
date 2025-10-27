8f01bffc8806a4bd8ff8deac3c96c83b11340b98
Deval <deval@arc10.io>
2025-10-27 17:22:31 -0400

    right way to remove prefix

--- application/service/upgrade_service.py
+++ application/service/upgrade_service.py
@@ -53,8 +53,8 @@ class UpgradeService:
         self, current_version: str, desired_version: str
     ) -> Tuple[bool, str, str]:
         """Compare system and current versions"""
-        current_v = current_version.lstrip("v").lstrip("provider-").lstrip("node-")
-        desired_v = desired_version.lstrip("v").lstrip("provider-").lstrip("node-")
+        current_v = current_version.lstrip("v").removeprefix("provider-").removeprefix("node-")
+        desired_v = desired_version.lstrip("v").removeprefix("provider-").removeprefix("node-")
 
         needs_upgrade = version.parse(current_v) < version.parse(desired_v)
 
